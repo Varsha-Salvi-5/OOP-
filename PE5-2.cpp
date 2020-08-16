40017843a31a6587d33fd47c399563b5c124f413
Varsha-Salvi <59506724+Varsha-Salvi-5@users.noreply.github.com>
2020-08-16 19:48:22 +0530

    Create PE5-2.cpp

---
+++ PE5-2.cpp
@@ -0,0 +1,25 @@
+/*
+Chapter No 5 Practice Exercise 2
+Q.Predict the output of the following program:
+
+
+*/
+
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+    char num[]="879K";
+    int res=atoi(num);
+    cout<<res;
+    cout<<endl<<"abs "<<abs(-9);
+    cout<<endl<<"ceil "<<ceil(2.7);
+    cout<<endl<<"floor is "<<floor(8.2);
+    cout<<endl<<"sqrt "<<sqrt (89.00);
+    return 0;
+}
+
+
+ 
