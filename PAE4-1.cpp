a6896db3d003ee23ba10d8c8915e7e6f78e6fea3
Varsha-Salvi <59506724+Varsha-Salvi-5@users.noreply.github.com>
2020-08-16 19:36:33 +0530

    Create PAE4-1.cpp

---
+++ PAE4-1.cpp
@@ -0,0 +1,23 @@
+/*Chapter No 4 Additional Exercise 1
+Predict the output of the following code:*/
+
+
+#include<iostream>
+using namespace std;
+
+main()
+{
+    int i=0;
+    int j=5;
+
+    for(;i<j;i++)
+    {
+        j--;
+
+    }
+          cout<<i<<endl;
+}
+
+
+
+ 
