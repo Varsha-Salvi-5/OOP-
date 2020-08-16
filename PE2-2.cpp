2c3b94857cdb7e920d4d8563bffa7abb5fde56b8
Varsha-Salvi <59506724+Varsha-Salvi-5@users.noreply.github.com>
2020-08-16 19:14:45 +0530

    Create PE2-2.cpp

---
+++ PE2-2.cpp
@@ -0,0 +1,22 @@
+//Chapter No 2 Practice Question No 2
+
+//Q.Predict the output of the following code:
+
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+
+char ch='A';
+char ch1=66;
+int i=60.00;
+
+cout<<ch<<endl<<ch1<<endl<<i;
+return 0;
+}
+
+
+
+ 
